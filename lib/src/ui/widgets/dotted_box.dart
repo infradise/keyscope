eed2afe8f7f319d9b3a84de6d3377ffaeb575770
Taeha Kim <taeha.kim@infradise.com>
2026-02-21 06:48:10 +0900

    feat(ui): add DottedBox to create New Connection

---
+++ lib/src/ui/widgets/dotted_box.dart
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2025-2026 Infradise Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'package:flutter/material.dart';
+
+class DottedBox extends StatelessWidget {
+  final double size;
+
+  const DottedBox({super.key, this.size = 80});
+
+  @override
+  Widget build(BuildContext context) => CustomPaint(
+        painter: _DottedBorderPainter(),
+        child: SizedBox(
+          width: size,
+          height: size,
+          child: const Center(
+            child: Icon(
+              Icons.add,
+              size: 48,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      );
+}
+
+class _DottedBorderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    const dashWidth = 6.0;
+    const dashSpace = 4.0;
+    final paint = Paint()
+      ..color = Colors.grey
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke;
+
+    double startX = 0;
+    while (startX < size.width) {
+      canvas.drawLine(
+        Offset(startX, 0),
+        Offset(startX + dashWidth, 0),
+        paint,
+      );
+      startX += dashWidth + dashSpace;
+    }
+
+    startX = 0;
+    while (startX < size.width) {
+      canvas.drawLine(
+        Offset(startX, size.height),
+        Offset(startX + dashWidth, size.height),
+        paint,
+      );
+      startX += dashWidth + dashSpace;
+    }
+
+    double startY = 0;
+    while (startY < size.height) {
+      canvas.drawLine(
+        Offset(0, startY),
+        Offset(0, startY + dashWidth),
+        paint,
+      );
+      startY += dashWidth + dashSpace;
+    }
+
+    startY = 0;
+    while (startY < size.height) {
+      canvas.drawLine(
+        Offset(size.width, startY),
+        Offset(size.width, startY + dashWidth),
+        paint,
+      );
+      startY += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
